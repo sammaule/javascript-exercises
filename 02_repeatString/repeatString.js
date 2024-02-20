aa58f09c7a4680e7ca58e9ec5cbf588164fa5d3b
Sam Maule <sam.maule@gmail.com>
2024-02-20 21:58:11 +0000

    exercises 1-6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatTimes) {
+  if (repeatTimes < 0) {
+    return 'ERROR';
+  }
+  let result = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    result += inputString;
+  }
+  return result;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        while (array.includes(args[i])) {
+            array.splice(array.indexOf(args[i]), 1);
+        }
+}
+    return array;
 };
 
 // Do not edit below this line
