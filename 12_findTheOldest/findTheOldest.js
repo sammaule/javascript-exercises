405d7dcc5095993342ad2024d7c384ec733e3b90
Sam Maule <smaule@deloitte.co.uk>
2024-08-22 11:33:34 +0100

    Answers to exercises 08-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr
+        .map(person => {
+            if (!person.yearOfDeath) {
+                person.yearOfDeath = new Date().getFullYear();
+            }
+            person.age = person.yearOfDeath - person.yearOfBirth;
+            return person;
+        })
+        .reduce((oldest, current) => {
+            return oldest.age > current.age ? oldest : current;
+        });
 };
 
 // Do not edit below this line
